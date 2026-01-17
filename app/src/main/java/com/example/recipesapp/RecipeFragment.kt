aa6f8ad85d52cbb6ae1ac9e12c4da64a789264af
Nikita Novgortcev <n42730689@gmail.com>
2026-01-17 21:09:46 +0300

    RA-12-Имплементировал интерфейс Parcelable

--- app/src/main/java/com/example/recipesapp/RecipeFragment.kt
+++ app/src/main/java/com/example/recipesapp/RecipeFragment.kt
@@ -1,21 +1,48 @@
 package com.example.recipesapp
 
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import com.example.recipesapp.constants.ARG_RECIPE
+import com.example.recipesapp.databinding.FragmentListCategoriesBinding
+import com.example.recipesapp.databinding.FragmentRecipeBinding
 
 class RecipeFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private var _binding: FragmentRecipeBinding? = null
+
+    private var recipe: Recipe? = null
+
+    private val binding
+        get() = _binding
+            ?: throw IllegalStateException("Binding for FragmentRecipeBinding must not be null")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_recipe, container, false)
+    ): View {
+        _binding = FragmentRecipeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        recipe = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+           arguments?.getParcelable(ARG_RECIPE, Recipe::class.java)
+        } else {
+            @Suppress("DEPRECATION")
+            arguments?.getParcelable(ARG_RECIPE)
+        }
+        binding.tvTitleRecipe.text = recipe?.title
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
--- app/src/main/java/com/example/recipesapp/RecipesListFragment.kt
+++ app/src/main/java/com/example/recipesapp/RecipesListFragment.kt
@@ -5,12 +5,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
 import com.example.recipesapp.constants.ARG_CATEGORY_ID
 import com.example.recipesapp.constants.ARG_CATEGORY_IMAGE_URL
 import com.example.recipesapp.constants.ARG_CATEGORY_NAME
+import com.example.recipesapp.constants.ARG_RECIPE
 import com.example.recipesapp.databinding.FragmentListRecipesBinding
 import com.example.recipesapp.recipes.STUB
 
@@ -64,8 +66,12 @@ class RecipesListFragment : Fragment() {
     }
 
     private fun openRecipeByRecipeId(recipeId: Int) {
+        val recipe = STUB.getRecipeById(recipeId)
+        val bundle = bundleOf(
+            ARG_RECIPE to recipe
+        )
         parentFragmentManager.commit {
-            replace<RecipeFragment>(R.id.mainContainer)
+            replace<RecipeFragment>(R.id.mainContainer,args = bundle)
             setReorderingAllowed(true)
             addToBackStack(null)
         }
