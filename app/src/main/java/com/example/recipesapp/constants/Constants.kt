395b1686093f8e7dbc41029554e984c8d08b6422
Nikita Novgortcev <n42730689@gmail.com>
2026-03-31 15:48:58 +0300

    RA-36 удолил константу

--- app/src/main/java/com/example/recipesapp/constants/Constants.kt
+++ app/src/main/java/com/example/recipesapp/constants/Constants.kt
@@ -7,4 +7,3 @@ const val SAVE_FAVORITES_ID = "save_favorites"
 const val IMAGE_URL = "https://recipes.androidsprint.ru/api/images/"
 const val IMAGE_BCG_CATEGORIES = "bcg_categories.png"
 const val IMAGE_BCG_FAVORITES = "bcg_favorites.png"
-const val DATA_ERROR = R.string.data_error
